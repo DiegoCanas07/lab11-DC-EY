b0caa5e3e05978f92d9fa521f3089ccd13a026b7
diego_canas <dcanasmgaj@gmail.com>
2025-11-12 16:33:18 -0500

    Calc functions done

--- calculator.py
+++ calculator.py
@@ -4,8 +4,36 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
+
+#https://github.com/DiegoCanas07/lab11-DC-EY.git
+#Partner 1: Diego Canas
+#Partner 2: Ethan Yin
+
+import math
+
+
 def add(a, b): 
-    pass
+    return a + b
+
+def sub(a,b):
+    return a - b
+
+def mul(a,b):
+    return a * b
+
+def div(a,b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    else:
+        return b/a
+
+def log(a,b):
+    if a <= 0 or b <= 0 or b == 1:
+        raise ValueError("Invalid logarithmic arguments")
+    else:
+        return math.log(b,a)
+
+def exp(a,b):
+    return a**b
 
 
