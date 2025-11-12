1b6eebcc22d9e34264ac724cd193977e87f112d3
Ethan Yin <ethanyin0109@gmail.com>
2025-11-12 17:47:22 -0500

    commited

--- calculator.py
+++ calculator.py
@@ -20,13 +20,13 @@ def hypotenuse(a, b):
 def add(a, b): 
     return a + b
 
-def sub(a,b):
+def subtract(a,b):
     return a - b
 
 def mul(a,b):
     return a * b
 
-def log(a,b):
+def logarithm(a,b):
     if a <= 0 or b <= 0 or b == 1:
         raise ValueError("Invalid logarithmic arguments")
     else:
